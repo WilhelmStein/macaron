93fd786bc82a068f0004bef42b2f5313490b8230
WilhelmStein <ioannis.cheilaris@gmail.com>
2020-09-22 12:58:06 +0300

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 echo "Creating virtual environment..."
 python3 -m venv env
-echo "Virtual environment created."
+echo "Virtual environment created.\n"
 source env/bin/activate
 echo "Installing dependencies..."
 pip3 install -r requirements.txt
-echo "Dependencies installed."
-deactivate
+printf "Dependencies installed.\nTo enter the virtual environment, type 'source env/bin/activate'\nThen, you can run the Macaron Explorer with \'python3 macaron_shell.py\'.\nTo exit the virtual environment, type 'deactivate'.\n\n"
